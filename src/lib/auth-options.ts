da936010c14b241d19edd970bf1a1adb85e78577
User <user@example.com>
2026-09-04 08:01:05 -0700

    Add trustHost: true to NextAuth config for Vercel

--- src/lib/auth-options.ts
+++ src/lib/auth-options.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 import { prisma } from "./prisma";
 
 export const authOptions: NextAuthOptions = {
+  trustHost: true,
   providers: [
     CredentialsProvider({
       name: "credentials",
